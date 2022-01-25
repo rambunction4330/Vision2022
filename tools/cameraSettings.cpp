22c2fed96f6ab2d71fd11e0f4b5de23349c08fb7
gcjurgiel <gcjurgiel@icloud.com>
2022-01-25 15:58:06 -0600

    Camera Setting configuator initial comit

---
+++ tools/cameraSettings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <filesystem>
+
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+
+int main(int argc, char* argv[]) {
+  return 0;
+}
